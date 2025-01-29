a32fd995aaaeafe3e9b44a518a0e5465b911eb75
Derek Bender <170351+djbender@users.noreply.github.com>
2025-01-29 08:01:25 -0800

    Change up approach to use enums

--- cards.c
+++ cards.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const char *CARD_VALUES[] = {
+typedef enum {
+  ACE = 1,
+  TWO,   // 2
+  THREE, // etc
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JACK,
+  QUEEN,
+  KING // 13
+} Rank;
+
+const int RankLength = 13;
+const char *RankNames[] = {
+  "Ace",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Jack",
+  "Queen",
+  "King"
+};
+
+const char *ShortRankNames[] = {
+  "A",
   "2",
   "3",
   "4",
@@ -10,33 +45,82 @@ const char *CARD_VALUES[] = {
   "8",
   "9",
   "10",
-  "JACK",
-  "QUEEN",
-  "KING",
-  "ACE"
+  "J",
+  "Q",
+  "K"
 };
-const int CARD_SIZE = sizeof(CARD_VALUES) / sizeof(CARD_VALUES[0]);
 
-const char *SUITS[] = {
-  "CLUB",
-  "DIAMOND",
-  "HEART",
-  "SPADE"
+// since Rank is 1-indexed, might as well do the same with Suit for symmetry
+typedef enum {
+  CLUBS = 1,
+  DIAMONDS = 2,
+  HEARTS = 3,
+  SPADES = 4
+} Suit;
+
+const int SuitLength = 4;
+const char *SuitNames[] = {
+  "Clubs",
+  "Diamonds",
+  "Hearts",
+  "Spades"
 };
-const int SUITS_SIZE = sizeof(SUITS) / sizeof(SUITS[0]);
+
+const char *ShortSuitNames[] = {
+  "♣",
+  "♡",
+  "♠",
+  "♢"
+};
+
+typedef struct card {
+  Rank rank;
+  Suit suit;
+} card;
+
+const char* getRankName(int r) {
+  return RankNames[r - 1];
+}
+
+const char* getSuitName(int s) {
+  return SuitNames[s];
+}
+
+const char* getShortRankName(int r) {
+  return ShortRankNames[r - 1];
+}
+
+const char* getShortSuitName(int s) {
+  return ShortSuitNames[s];
+}
+
+void printCard(card c) {
+  printf(
+    "%s%s ",
+    getShortRankName(c.rank),
+    getShortSuitName(c.suit)
+  );
+}
+
+void printDeck(card *deck) {
+  for(int c = 0; c < 52; c++) {
+    if(deck[c].rank == 0) {
+      break;
+    }
+    printCard(deck[c]);
+  }
+  puts("");
+}
 
 int main() {
-
-  printf("size: %d\n", (int)(sizeof(CARD_VALUES) / sizeof(CARD_VALUES[0])));
-  for(int i = 0; i < CARD_SIZE; i++) {
-    printf("Entry: %s\n", CARD_VALUES[i]);
-  }
-  for(int i = 0; i < SUITS_SIZE; i++) {
-    printf("Entry: %s\n", SUITS[i]);
-  }
-  for(int i = 0; i < CARD_SIZE; i++) {
-    for(int j = 0; j < SUITS_SIZE; j++) {
-      printf("Card: %s of %s\n", CARD_VALUES[i], SUITS[j]);
+  card *startingDeck = malloc(52 * sizeof(card));
+  int index = 0;
+  for(int r = 1; r <= RankLength; r++) {
+    for(int s = 1; s <= SuitLength; s++) {
+      card myCard = { r, s-1 };
+      startingDeck[index++] = myCard;
+      printDeck(startingDeck);
     }
   }
+  free(startingDeck);
 }
