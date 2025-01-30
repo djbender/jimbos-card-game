63c3c34e77295e75de720705f57618ceaad95a81
Derek Bender <170351+djbender@users.noreply.github.com>
2025-01-29 16:03:30 -0800

    Adopt kernel.org styling

--- cards.c
+++ cards.c
@@ -1,127 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {
-  ACE = 1,
-  TWO,   // 2
-  THREE, // etc
-  FOUR,
-  FIVE,
-  SIX,
-  SEVEN,
-  EIGHT,
-  NINE,
-  TEN,
-  JACK,
-  QUEEN,
-  KING // 13
-} Rank;
-
-const int RankLength = 13;
-const char *RankNames[] = {
-  "Ace",
-  "Two",
-  "Three",
-  "Four",
-  "Five",
-  "Six",
-  "Seven",
-  "Eight",
-  "Nine",
-  "Ten",
-  "Jack",
-  "Queen",
-  "King"
+enum Rank {
+        ACE = 1,
+        TWO,   // 2
+        THREE, // etc
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING // 13
 };
 
-const char *ShortRankNames[] = {
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
+const int RANK_LENGTH = 13;
+const char *RANK_NAMES[] = {
+        "Ace",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Jack",
+        "Queen",
+        "King"
+};
+
+const char *SHORT_RANK_NAMES[] = {
+        "A",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K"
 };
 
 // since Rank is 1-indexed, might as well do the same with Suit for symmetry
-typedef enum {
-  CLUBS = 1,
-  DIAMONDS = 2,
-  HEARTS = 3,
-  SPADES = 4
-} Suit;
+enum Suit {
+        CLUBS = 1,
+        DIAMONDS = 2,
+        HEARTS = 3,
+        SPADES = 4
+};
 
-const int SuitLength = 4;
+const int SUIT_LENGTH = 4;
 const char *SuitNames[] = {
-  "Clubs",
-  "Diamonds",
-  "Hearts",
-  "Spades"
+        "Clubs",
+        "Diamonds",
+        "Hearts",
+        "Spades"
 };
 
-const char *ShortSuitNames[] = {
-  "♣",
-  "♡",
-  "♠",
-  "♢"
+const char *SHORT_SUIT_NAMES[] = {
+        "♣",
+        "♡",
+        "♠",
+        "♢"
 };
 
-typedef struct card {
-  Rank rank;
-  Suit suit;
-} card;
+struct card {
+        enum Rank rank;
+        enum Suit suit;
+};
 
-const char* getRankName(int r) {
-  return RankNames[r - 1];
+const char* rank_name(int r) {
+        return RANK_NAMES[r - 1];
 }
 
-const char* getSuitName(int s) {
-  return SuitNames[s];
+const char* suit_name(int s) {
+        return SuitNames[s];
 }
 
-const char* getShortRankName(int r) {
-  return ShortRankNames[r - 1];
+const char* short_rank_name(int r) {
+        return SHORT_RANK_NAMES[r - 1];
 }
 
-const char* getShortSuitName(int s) {
-  return ShortSuitNames[s];
+const char* short_suit_name(int s) {
+        return SHORT_SUIT_NAMES[s];
 }
 
-void printCard(card c) {
-  printf(
-    "%s%s ",
-    getShortRankName(c.rank),
-    getShortSuitName(c.suit)
-  );
+void print_card(struct card c) {
+        printf(
+                        "%s%s ",
+                        short_rank_name(c.rank),
+                        short_suit_name(c.suit)
+              );
 }
 
-void printDeck(card *deck) {
-  for(int c = 0; c < 52; c++) {
-    if(deck[c].rank == 0) {
-      break;
-    }
-    printCard(deck[c]);
-  }
-  puts("");
+void print_deck(struct card *deck) {
+        for(int c = 0; c < 52; c++) {
+                if(deck[c].rank == 0) {
+                        break;
+                }
+                print_card(deck[c]);
+        }
+        puts("");
 }
 
 int main() {
-  card *startingDeck = malloc(52 * sizeof(card));
-  int index = 0;
-  for(int r = 1; r <= RankLength; r++) {
-    for(int s = 1; s <= SuitLength; s++) {
-      card myCard = { r, s-1 };
-      startingDeck[index++] = myCard;
-      printDeck(startingDeck);
-    }
-  }
-  free(startingDeck);
-  return 0;
+        struct card *startingDeck = malloc(52 * sizeof(struct card));
+        int index = 0;
+        for(int r = 1; r <= RANK_LENGTH; r++) {
+                for(int s = 1; s <= SUIT_LENGTH; s++) {
+                        struct card myCard = { r, s-1 };
+                        startingDeck[index++] = myCard;
+                        print_deck(startingDeck);
+                }
+        }
+        free(startingDeck);
+        return 0;
 }
